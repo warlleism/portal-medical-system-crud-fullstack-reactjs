92636efd81d250babdb0f855876a8af4a7e2a5cd
Warllei Martins <warllei.s.martins@hotmail.com>
2023-01-08 13:30:38 -0300

    correções

--- src/main/index.jsx
+++ src/main/index.jsx
@@ -32,131 +32,144 @@ const Main = () => {
             </div>
             <div className="list-links-field">
 
-                <div className="link-field" onClick={(event) => {
-                    alterData(event.target)
-                    setConsultas(!consultas)
-                }}>
-                    <div className="title-icon-link-field">
-                        <span className="material-symbols-outlined">
-                            content_paste
-                        </span>
-                        <div>Consultas</div>
-                    </div>
-                    <i className="material-symbols-outlined">
-                        arrow_drop_down
-                    </i>
-                </div>
-                {
-                    consultas
-                        ?
-                        <>
-                            <div className="link-field">
-                                <div>Cadastrar</div>
-                            </div>
-                            <div className="link-field">
-                                <div>Visualizar</div>
-                            </div>
-                            <div className="link-field">
-                                <div>Editar</div>
-                            </div>
-                        </>
-                        :
-                        false
-                }
-                <div className="link-field"
-                    onClick={(event) => {
+                <div>
+                    <div className="link-field" onClick={(event) => {
                         alterData(event.target)
-                        setMedicos(!medicos)
+                        setConsultas(!consultas)
                     }}>
-                    <div className="title-icon-link-field">
-                        <span className="material-symbols-outlined">
-                            vaccines
-                        </span>
-                        <div>Médicos</div>
+                        <div className="title-icon-link-field">
+                            <span className="material-symbols-outlined">
+                                content_paste
+                            </span>
+                            <div>Consultas</div>
+                        </div>
+                        <i className="material-symbols-outlined">
+                            arrow_drop_down
+                        </i>
                     </div>
-                    <i className="material-symbols-outlined">
-                        arrow_drop_down
-                    </i>
+                    {
+                        consultas
+                            ?
+                            <div className="container-field">
+                                <div className="field">
+                                    <div>Cadastrar</div>
+                                </div>
+                                <div className="field">
+                                    <div>Visualizar</div>
+                                </div>
+                                <div className="field">
+                                    <div>Editar</div>
+                                </div>
+                            </div>
+                            :
+                            false
+                    }
                 </div>
-                {
-                    medicos
-                        ?
-                        <>
-                            <div className="link-field">
-                                <div>Cadastrar</div>
-                            </div>
-                            <div className="link-field">
-                                <div>Visualizar</div>
-                            </div>
-                            <div className="link-field">
-                                <div>Editar</div>
-                            </div>
-                        </>
-                        :
-                        false
-                }
-                <div className="link-field" onClick={(event) => {
-                    alterData(event.target)
-                    setPacientes(!pacientes)
-                }}>
-                    <div className="title-icon-link-field">
-                        <span className="material-symbols-outlined">
-                            person
-                        </span>
-                        <div>Pacientes</div>
+
+                <div>
+                    <div className="link-field"
+                        onClick={(event) => {
+                            alterData(event.target)
+                            setMedicos(!medicos)
+                        }}>
+                        <div className="title-icon-link-field">
+                            <span className="material-symbols-outlined">
+                                vaccines
+                            </span>
+                            <div>Médicos</div>
+                        </div>
+                        <i className="material-symbols-outlined">
+                            arrow_drop_down
+                        </i>
                     </div>
-                    <i className="material-symbols-outlined">
-                        arrow_drop_down
-                    </i>
+
+                    {
+                        medicos
+                            ?
+                            <div className="container-field">
+                                <div className="field">
+                                    <div>Cadastrar</div>
+                                </div>
+                                <div className="field">
+                                    <div>Visualizar</div>
+                                </div>
+                                <div className="field">
+                                    <div>Editar</div>
+                                </div>
+                            </div>
+                            :
+                            false
+                    }
                 </div>
-                {
-                    pacientes
-                        ?
-                        <>
-                            <div className="link-field">
-                                <div>Cadastrar</div>
-                            </div>
-                            <div className="link-field">
-                                <div>Visualizar</div>
-                            </div>
-                            <div className="link-field">
-                                <div>Editar</div>
-                            </div>
-                        </>
-                        :
-                        false
-                }
-                <div className="link-field" onClick={(event) => {
-                    alterData(event.target)
-                    setProcedimentos(!procedimentos)
-                }}>
-                    <div className="title-icon-link-field">
-                        <span className="material-symbols-outlined">
-                            stethoscope
-                        </span>
-                        <div>Pricedimentos</div>
+
+                <div>
+                    <div className="link-field" onClick={(event) => {
+                        alterData(event.target)
+                        setPacientes(!pacientes)
+                    }}>
+                        <div className="title-icon-link-field">
+                            <span className="material-symbols-outlined">
+                                person
+                            </span>
+                            <div>Pacientes</div>
+                        </div>
+                        <i className="material-symbols-outlined">
+                            arrow_drop_down
+                        </i>
                     </div>
-                    <i className="material-symbols-outlined">
-                        arrow_drop_down
-                    </i>
+                    {
+                        pacientes
+                            ?
+                            <div className="container-field">
+                                <div className="field">
+                                    <div>Cadastrar</div>
+                                </div>
+                                <div className="field">
+                                    <div>Visualizar</div>
+                                </div>
+                                <div className="field">
+                                    <div>Editar</div>
+                                </div>
+                            </div>
+                            :
+                            false
+                    }
                 </div>
-                {
-                    procedimentos
-                        ?
-                        <>
-                            <div className="link-field">
-                                <div>Cadastrar</div>
+
+                <div>
+                    <div className="link-field" onClick={(event) => {
+                        alterData(event.target)
+                        setProcedimentos(!procedimentos)
+                    }}>
+                        <div className="title-icon-link-field">
+                            <span className="material-symbols-outlined">
+                                stethoscope
+                            </span>
+                            <div>Pricedimentos</div>
+                        </div>
+                        <i className="material-symbols-outlined">
+                            arrow_drop_down
+                        </i>
+                    </div>
+                    {
+                        procedimentos
+                            ?
+                            <div className="container-field">
+                                <div className="field">
+                                    <div>Cadastrar</div>
+                                </div>
+                                <div className="field">
+                                    <div>Visualizar</div>
+                                </div>
+                                <div className="field">
+                                    <div>Editar</div>
+                                </div>
                             </div>
-                            <div className="link-field">
-                                <div>Visualizar</div>
-                            </div>
-                            <div className="link-field">
-                                <div>Editar</div>
-                            </div>
-                        </>
-                        :
-                        false
-                }
+                            :
+                            false
+                    }
+                </div>
+
                 <div className="link-field">
                     <div className="title-icon-link-field">
                         <span className="material-symbols-outlined">
