9a145e08572f32c58c96c7b37f4043dfedac8ecd
Warllei Martins <warllei.s.martins@hotmail.com>
2023-01-11 12:30:43 -0300

    correção na api/rota de criar novo especialista

--- backend/src/controller/especialista/create-especialista.js
+++ backend/src/controller/especialista/create-especialista.js
@@ -4,15 +4,16 @@ const especialidades = require('../../model/regis-especialidades')
 const criarNovoEspecialista = async (req, res) => {
     var { nome, especialidade, contato } = req.body;
 
-    const user = await especialista.findOne({ where: { nome, especialidade } })
+    const esp = await especialidades.findOne({ where: { especialidade } })
 
-    if (user) {
-        return res.send({ status: 200, sucess: 'Especialista já cadastrada.' });
+    if (esp) {
+        const criarEspecialista = await especialista.create({ nome, especialidade, contato })
+        return res.status(200).send({ status: 200, sucess: 'Especialista cadastrado com sucesso' });
     }
 
     try {
         const criarEspecialista = await especialista.create({ nome, especialidade, contato })
-        const criarEspecialidade = await especialidades.create({  especialidade })
+        const criarEspecialidade = await especialidades.create({ especialidade })
         return res.status(200).send({ status: 200, sucess: 'Especialista cadastrado com sucesso' });
     } catch (err) {
         res.status(400).send({ error: 'Algo deu errado! ' + err });
