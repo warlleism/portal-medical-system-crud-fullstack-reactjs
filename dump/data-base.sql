025e585a1a4f1c98691ae82ae734966bb53ccd21
Warllei Martins <warllei.s.martins@hotmail.com>
2023-01-11 11:53:36 -0300

    dump atualizado

--- dump/data-base.sql
+++ dump/data-base.sql
@@ -35,7 +35,7 @@ CREATE TABLE `consultas` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,7 +44,7 @@ CREATE TABLE `consultas` (
 
 LOCK TABLES `consultas` WRITE;
 /*!40000 ALTER TABLE `consultas` DISABLE KEYS */;
-INSERT INTO `consultas` VALUES (18,'Warllei Martins','Roger Abdelmassih','Dermatologia','27995804151','2023-03-04','16:30','2023-01-09 19:24:29','2023-01-09 19:24:29');
+INSERT INTO `consultas` VALUES (40,'Warllei Martins','José Eduardo Souza','Dermatologia','(27) 99580-4151','2023-03-04','15:30','2023-01-10 17:28:07','2023-01-10 17:28:07'),(42,'Gabriel Santos','Dr Eron Junior','Dermatologia','(27) 99580-4151','2023-03-03','12:00','2023-01-10 20:08:38','2023-01-10 20:08:38');
 /*!40000 ALTER TABLE `consultas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,11 +57,11 @@ DROP TABLE IF EXISTS `especialidades`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `especialidades` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
+  `especialidade` varchar(100) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,7 +70,7 @@ CREATE TABLE `especialidades` (
 
 LOCK TABLES `especialidades` WRITE;
 /*!40000 ALTER TABLE `especialidades` DISABLE KEYS */;
-INSERT INTO `especialidades` VALUES (1,'Dermatologia','2023-01-09 12:58:54','2023-01-09 12:58:54'),(2,'Neurologia','2023-01-09 16:10:37','2023-01-09 16:10:37');
+INSERT INTO `especialidades` VALUES (1,'Neurologista','2023-01-11 14:43:44','2023-01-11 14:51:44');
 /*!40000 ALTER TABLE `especialidades` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -89,7 +89,7 @@ CREATE TABLE `especialistas` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,7 +98,7 @@ CREATE TABLE `especialistas` (
 
 LOCK TABLES `especialistas` WRITE;
 /*!40000 ALTER TABLE `especialistas` DISABLE KEYS */;
-INSERT INTO `especialistas` VALUES (1,'José Eduardo Souza','Dermatologia','27995804151','2023-01-09 16:27:37','2023-01-09 18:05:04'),(2,'Magdi Yacoub','Neurologia','(27) 3122-5678','2023-01-09 16:28:08','2023-01-09 16:28:08'),(3,'David Uip','Neurologia','(27) 3122-5678','2023-01-09 16:28:59','2023-01-09 16:28:59'),(4,'Roger Abdelmassih','Dermatologia','(27) 3122-5678','2023-01-09 16:29:12','2023-01-09 16:29:12'),(5,'William Harvey','Dermatologia','(27) 3122-5678','2023-01-09 16:29:17','2023-01-09 16:29:17'),(6,'Zezé Polessa','Neurologia','(27) 3122-5678','2023-01-09 16:29:32','2023-01-09 16:29:32');
+INSERT INTO `especialistas` VALUES (1,'Maria Fernanda Peixoto','Dermatologia','(27) 2691-5615','2023-01-11 13:24:48','2023-01-11 13:24:48'),(2,'Lívia Teixeira','Cirurgião geral','(96) 2716-1731','2023-01-11 13:37:58','2023-01-11 13:37:58'),(3,'Isabella Correia','Endocrinologia','(68) 3246-7375','2023-01-11 13:40:03','2023-01-11 13:40:03'),(4,'João Pedro Freitas','Dermatologia','(77) 2445-3345','2023-01-11 13:40:46','2023-01-11 13:40:46'),(5,'Gilmar Galvão','Dermatologia','(27) 99580-4151','2023-01-11 13:57:11','2023-01-11 13:57:11'),(6,'Fabio Fernandes','Dermatologia','(27) 99580-4151','2023-01-11 13:58:12','2023-01-11 13:58:12'),(7,'João Alberto','Neurologia','(27) 33879-4823','2023-01-11 14:39:25','2023-01-11 14:39:25'),(8,'João Alberto','Neurologista','(27) 99580-4151','2023-01-11 14:43:43','2023-01-11 14:43:43');
 /*!40000 ALTER TABLE `especialistas` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -111,4 +111,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-01-09 20:19:27
+-- Dump completed on 2023-01-11 11:53:08
